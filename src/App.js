b9c62d66e49612b828d6353149f8535dec61ad3c
Barbara <barbara.prusiewicz@gmail.com>
2025-05-22 07:42:25 -1000

    Remove bootstrap dependencies and packages

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React from 'react';
 
 import DesignDashboard from './components/designDashboard';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'jquery/dist/jquery.min.js';
-import 'bootstrap/dist/js/bootstrap.min.js';
 import './App.css';
 
 import { LicenseInfo } from '@mui/x-license';
